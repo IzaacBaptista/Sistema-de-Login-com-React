ad9d075f0b869ec0e84f88494ee0ea4c675b2621
Izaac Baptista <izaacbaptista@gmail.com>
2022-05-12 21:11:49 -0300

    component-style

---
+++ appregistro/src/components/styles.js
@@ -0,0 +1,13 @@
+// components/styles.js
+// yarn add twrnc
+import tw from 'twrnc'
+
+const styles = {
+    container: tw`flex flex-1 items-center justify-center`,
+    sub_container: tw`bg-white p-5 rounded-xl min-w-[360px]`,
+    button_split: tw`flex justify-between flex-row `,
+    input: tw`p-2 rounded-xl border border-gray border-solid my-5`,
+    botao: tw`p-2 rounded-xl border border-gray border-solid bg-green my-5`,
+}
+
+export default styles
